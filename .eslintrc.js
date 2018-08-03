96a77aa95c1019528df62fa2061fd7f9c22654bb
Roman Sluka <r.sluka@itransition.com>
2018-08-03 12:28:37 +0300

    added AuthInput component, plugins for images, and hook for styled components

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "parser": "babel-eslint",
+    rules:{
+        "linebreak-style": 0,
+        "indent": ["error", 2],
+        "func-names": ["error", "never"],
+        "object-curly-newline": ["error", {
+            "ImportDeclaration": "never",
+        }],
+        "react/jsx-one-expression-per-line": 0,
+    }
+};
---
+++ client/src/components/AuthWindow/style.js
@@ -0,0 +1,94 @@
+import styled, { injectGlobal } from 'styled-components';
+import Link from 'next/link';
+import user from '../../images/user.svg';
+import foreign from '../../images/foreign.png';
+import bg from '../../images/bg.jpg';
+import userRoles from '../../constants/userRoles';
+
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,900|Roboto');
+
+  body {
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    font-family: Roboto, sans-serif;
+    // background: linear-gradient(to bottom,#1f50daeb,#bbf0f9);
+    background-image: url(${bg});
+    background-size: 100% 100%;
+    user-select: none;
+  }
+
+  h1 {
+    font-family: Montserrat;
+  }
+
+  .success-message {
+    text-align: center;
+    color: #5b945b;
+    font-weight: bold;
+    font-size: 16px;
+  }
+`;
+
+export const AuthLink = styled(Link)`
+    text-decoration: none;
+    width: 40%;
+    margin: 0 auto;
+    &:focus, &:hover, &:visited, &:link, &:active {
+        text-decoration: none;
+    }
+    color: #00bcd4;
+`;
+
+export const UserImg = styled.figure`
+  background-image: url(${({ userRole }) => (userRole === userRoles.user ? user : foreign)});
+  background-size: 100px 100px;
+  background-repeat: no-repeat;
+  background-color: rgba(255,255,255,0.61);
+  width:100px;
+  height: 100px;
+  margin: 20px auto;
+`;
+
+export const SignView = styled.section`
+  // min-width: 250px;
+  // max-width: 385px;
+  min-width: 250px;
+  max-width: 500px;
+  width: 50%;
+  min-height: 500px;
+  background-color: #000000bd;
+  // background: linear-gradient(to bottom,#fbfbfb,#ffffff69);
+  // border-radius: 15%;
+  Box-shadow: 0px 0px 260px 60px rgba(169,159,151,0.26);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const LoginBtn = styled.button`
+  border: 1px solid #c8c6c6;
+  background-color: #1db387;
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+  color: white;
+  width: 31%;
+  margin: 12px auto;
+  border-radius: 15px;
+  cursor: pointer;
+  outline: none;
+  &:active {
+    background-color: #13886680;
+  }
+`;
+
+export const AuthQuestion = styled.p`  
+  text-align: center;
+  color: rgba(255,255,255,0.61);
+`;
